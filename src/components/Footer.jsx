4485285632890f781637ee638d9ab94747b13a61
heshameldawy <Heshameldawy__@outlook.com>
2023-07-03 02:29:35 -0400

    updating logo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -79,7 +79,7 @@ const Footer = () => {
       <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
         <div className="flex-[1] flex flex-col justify-start mr-10">
           <img
-            src=""
+            src="/logo.png"
             alt="hoobank"
             className="w-[266px] h-[72.14px] object-contain"
           />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
 
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
-      <img src="" alt="my logo" className="w-[124px] h-[32px]" />
+      <img src="/logo.png" alt="my logo" className="w-[124px] h-[32px]" />
 
       <ul className="list-none sm:flex hidden justify-end items-center flex-1">
         {navLinks.map((nav, index) => (
