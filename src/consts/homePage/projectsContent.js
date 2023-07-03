7054ab23b635e692a1ea4a1523b3d204dc35c175
heshameldawy <Heshameldawy__@outlook.com>
2023-07-03 02:22:07 -0400

    landing page done

---
+++ src/consts/homePage/projectsContent.js
@@ -0,0 +1,16 @@
+export const projectsCardInfo = [
+  {
+    content: `The application to summarize articles by pasting the url in the input form
+      Connecting with AI API for summarizing the articles.
+      Using Redux to connect to the API and to get specific data from the call`,
+    title: "AI Summrizer",
+    img: "/projectsImgs/AIsummrizer.png"
+  },
+  {
+    content:
+      "This is a full stack application where the vehicles information been loaded from an API to show all the cars with a search bar.",
+    title: "Car dealership",
+    img: "/projectsImgs/carDealer.png"
+  }
+  // Add more card objects as needed
+];
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,60 +5,10 @@ import InfoLine from "../components/infoLine";
 import Footer from "../components/Footer";
 import ContentBox from "../components/contentBox";
 import BlogContentBox from "../components/BlogContentBox";
+import { blogData } from "../consts/homePage/blogsContent";
+import { projectsCardInfo } from "../consts/homePage/projectsContent";
 const HomePage = () => {
   const stats = [{ title: "Years of Experience", value: "4+" }];
-  const projectsCardInfo = [
-    {
-      content: `The application to summarize articles by pasting the url in the input form
-      Connecting with AI API for summarizing the articles.
-      Using Redux to connect to the API and to get specific data from the call`,
-      title: "AI Summrizer",
-      img: "/projectsImgs/AIsummrizer.png"
-    },
-    {
-      content:
-        "This is a full stack application where the vehicles information been loaded from an API to show all the cars with a search bar.",
-      title: "Car dealership",
-      img: "/projectsImgs/carDealer.png"
-    }
-    // Add more card objects as needed
-  ];
-  const blogData = [
-    {
-      content: `In this article, I explained the 4 pillars in OOP which they are Abstraction, Inheritance, Encapsulation, Polymorphism.</br>
-      In OOP, we bring together the data and methods that operate on it in a single Object.</br>
-      OOP programming paradigm is often a better choice for creating graphical user interfaces and games.</br>
-      <strong class="blue_gradient">Benfits of OOP</strong> 
-      <ul>
-       <li><strong>Reduces complexity:</strong> by breaking down a large complex application into smaller, more manageable and easier to maintain objects</li>
-       <li><strong>Easier Maintenance:</strong> Every part can work together and fix independently</li>
-       <li><strong>Reduces complexity:</strong> Create templets</li>
-       <li><strong>Reduces complexity:</strong> Less time building applications</li>
-      </ul>
-      `,
-      header: `<strong class="orange_gradient">OOP concepts</strong>`,
-      image:
-        "https://scaler.com/topics/images/object-oriented-programming-concepts-1024x870.jpg"
-    },
-    {
-      content:
-        "This is a full stack application where the vehicles information been loaded from an API to show all the cars with a search bar.",
-      header: "Car dealership",
-      image: "/projectsImgs/carDealer.png"
-    },
-    {
-      content:
-        "This is a full stack application where the vehicles information been loaded from an API to show all the cars with a search bar.",
-      header: "Car dealership",
-      image: "/projectsImgs/carDealer.png"
-    },
-    {
-      content:
-        "This is a full stack application where the vehicles information been loaded from an API to show all the cars with a search bar.",
-      header: "Car dealership",
-      image: "/projectsImgs/carDealer.png"
-    }
-  ];
   return (
     <div className="bg-black w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
